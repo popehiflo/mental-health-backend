0ae9f12567fec47dc7b754f33103962313a406a3
popehiflo <poolpetterhijuel@hotmail.com>
2022-03-25 05:21:37 -0500

    feat: crear User

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,24 @@
-const getUsers = (req, res) => {
+const UsuarioModel = require('../models/user.model');
+const getAllUsers = (req, res) => {
   res.json({
     ok: true,
     data: [],
   });
 };
 
-module.exports = { getUsers, };
+const createUser = async(req, res) => {
+  const { first_name, last_name, email, password } = req.body;
+
+  const usuario = new UsuarioModel(req.body);
+  await usuario.save();
+
+  res.json({
+    ok: true,
+    usuario,
+  });
+};
+
+module.exports = { 
+  getAllUsers, 
+  createUser 
+};
